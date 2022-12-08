405b2604ec1da2f2e4faff9437d9ba72369e1259
Yashodha Dilshan <yashodadilshan@gmail.com>
2022-12-08 14:13:28 +0530

    Create Login Form Gui

---
+++ src/Controllers/LoginFormController.java
@@ -0,0 +1,15 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class LoginFormController {
+    public void btnExitOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnResetOnAction(ActionEvent actionEvent) {
+    }
+}
