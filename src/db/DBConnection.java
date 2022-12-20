6ce7efd0937a076885f43c4ddf34fec332a8f370
Yashodha Dilshan <114228913+yashodhaDilshan@users.noreply.github.com>
2022-12-20 21:51:40 +0530

    Update database  to WampServer Mysql

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -11,7 +11,7 @@ public class DBConnection {
     public DBConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/citybookshop", "root", "1234");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/citybookshop", "root", "");
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
